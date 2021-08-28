8037fc9d5fdbc817ddeeefde762ae0aed0048781
Francesco <manzato.francesco@gmail.com>
2021-08-28 12:53:34 +0200

    first commit
    ..

---
+++ src/main/java/com/example/mobileNumbers/MobileNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobileNumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileNumbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mobileNumbers/MobileNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobileNumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
